1eaf2da7bc85c0e3b1b8e1551217d9080c0b7114
Anna <petunova@gmail.com>
2014-06-27 21:38:11 +0400

    hw13

---
+++ addressbook/run_my_test-suite.bat
@@ -0,0 +1,3 @@
+java -cp bin;D:\for-work\programming-for-testers\special-programs\xstream-distribution-1.4.7-bin\xstream-1.4.7\lib\xstream-1.4.7.jar;D:\for-work\programming-for-testers\special-programs\xstream-distribution-1.4.7-bin\xstream-1.4.7\lib\xstream\xmlpull-1.1.3.1.jar;D:\for-work\programming-for-testers\special-programs\xstream-distribution-1.4.7-bin\xstream-1.4.7\lib\xstream\xpp3_min-1.1.4c.jar;D:\for-work\programming-for-testers\special-programs\selenium-server-standalone-2.41.0.jar   org.testng.TestNG testng-customsuite.xml
+
+@pause
